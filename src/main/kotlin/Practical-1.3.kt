b165ed7e54e81007bb95e74f47c37c97dc04ab84
student <student@gnu.ac.in>
2023-07-19 08:51:57 +0530

    ok

---
+++ src/main/kotlin/Practical-1.3.kt
@@ -0,0 +1,21 @@
+fun main()
+{
+    var a = 22172012040;
+    var b = "Roshan";
+    var c = "CE";
+    var d = "CEIT-B";
+    var e = "B-3";
+    var f = "UVPCE";
+    var g = "GANPAT UNIVERSITY";
+    var h = "20";
+
+    println("Student Enrollment number : $a");
+    println("Student Name : $b");
+    println("Student Branch : $c");
+    println("Student Class : $d");
+    println("Student Batch : $e");
+    println("Student Collage : $f");
+    println("Student University : $g");
+    println("Student Age : $h");
+
+}
