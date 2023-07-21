5a44dbf4b90392d731cbd57fde9a6fe2dc5784bc
student <student@gnu.ac.in>
2023-07-21 12:29:21 +0530

    okk

--- src/main/kotlin/Practical-1.3.kt
+++ src/main/kotlin/Practical-1.3.kt
@@ -1,6 +1,29 @@
-fun main()
-{
-    println("EnRoll : ");
-    var sEnrollmentNumber=
+data class Student(
+    val name: String,
+    val age: Int,
+    val rollNumber: String,
+    val grade: Char
+)
 
+fun main() {
+    println("Enter student's data :")
+    print("Name: ")
+    val name = readLine() ?: ""
+
+    print("Age: ")
+    val age = readLine()?.toIntOrNull() ?: 0
+
+    print("Roll Number: ")
+    val rollNumber = readLine() ?: ""
+
+    print("Grade: ")
+    val grade = readLine()?.get(0)?.toUpperCase() ?: ' '
+
+    val student = Student(name, age, rollNumber, grade)
+
+    println("\nStudent's data :")
+    println("Name: ${student.name}")
+    println("Age: ${student.age}")
+    println("Roll Number: ${student.rollNumber}")
+    println("Grade: ${student.grade}")
 }
--- src/main/kotlin/Practical-1.4.kt
+++ src/main/kotlin/Practical-1.4.kt
@@ -1,6 +1,15 @@
-fun main()
-{
-    i = 4;
-    var i = if(i/2=0);
+fun main(args: Array<String>) {
+    println("Enter a number:")
+    val input = readLine()
+    val number = input?.toIntOrNull()
 
+    if (number != null) {
+        if (number % 2 == 0) {
+            println("$number is an even number.")
+        } else {
+            println("$number is an odd number.")
+        }
+    } else {
+        println("input a valid number")
+    }
 }
